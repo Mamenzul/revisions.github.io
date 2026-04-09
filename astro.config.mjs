9a5f8b53e38b5a731f0e5ee7ff50c7c13069cff6
Victor Brettes <victor.brettes@gmail.com>
2026-04-09 22:10:07 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://revisions.github.io',
+  base: 'revisions',
   integrations: [mdx()]
 });
